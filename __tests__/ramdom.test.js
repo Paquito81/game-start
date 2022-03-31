92a011c896c5c81dbcebc2b7e819138482cb3c5e
Paquito81 <95262020+Paquito81@users.noreply.github.com>
2022-03-31 06:16:09 -0700

    adding

---
+++ __tests__/ramdom.test.js
@@ -0,0 +1,7 @@
+const tandomNumber = require('../lib/random');
+
+test('generates a random number between 1 and 10', () => {
+    expect(randomNumber()).toBeGreaterThanOrEqual(1);
+    expect(randomNumber()).toBeLessThanOrEqual(10);
+
+})
