b72e358d5fffa2bdf9ecc9403bd8d194035c766a
Paquito81 <95262020+Paquito81@users.noreply.github.com>
2022-03-31 15:49:19 -0700

    debugged code

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,7 +1,8 @@
-const Potion = require('../lib/Potion');
+const Potion = require('../lib/Potion.js');
+const Player = require('../lib/Player.js');
 jest.mock('../lib/Potion.js');
 console.log(new Potion());
-const Player = require('../lib/Player');
+
 
 test('creates a player object', () => {
     const player = new Player('Dave');
@@ -10,9 +11,8 @@ test('creates a player object', () => {
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
-    expect(player.inventory).toEqual(
-        expect.arrayContaining([expect.any(Object)])
-    );
+
+    expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
 });
 
 test("gets player's stat as an object", () => {
@@ -20,7 +20,7 @@ test("gets player's stat as an object", () => {
     expect(player.getStats()).toHaveProperty('potions');
     expect(player.getStats()).toHaveProperty('health');
     expect(player.getStats()).toHaveProperty('strength');
-    expect(player.getStats()).toHaveProperty('agility')
+    expect(player.getStats()).toHaveProperty('agility');
 });
 
 test('gets inventory from player or return false', () => {
--- __tests__/ramdom.test.js
+++
@@ -1,7 +0,0 @@
-const tandomNumber = require('../lib/random');
-
-test('generates a random number between 1 and 10', () => {
-    expect(randomNumber()).toBeGreaterThanOrEqual(1);
-    expect(randomNumber()).toBeLessThanOrEqual(10);
-
-})
